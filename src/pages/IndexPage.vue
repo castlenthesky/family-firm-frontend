41a93857e6557797bcbe6385c31825e18dcda1aa
Brian Henson <brian.reid.henson@gmail.com>
2024-01-14 09:50:04 -0700

    plaid link token in sandbox

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="row">Welcome to the home page!!</div>
+
+  <PlaidLink
+    clientName="Vue Test"
+    env="sandbox"
+    public_key="dbaef5e48f1ccc027c209c029ef395"
+    :products="['auth', 'transactions']"
+    webhook="https://requestb.in"
+    :onSuccess="onSuccess"
+    :onExit="onExit"
+    :onEvent="onEvent"
+  >
+    <q-btn color="primary">Call Plaid Link</q-btn>
+  </PlaidLink>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PlaidLink from 'components/plaid/PlaidLink.vue';
+
+function onEvent(eventName: string, metadata: any) {
+  console.log(eventName);
+  return;
+}
+
+function onSuccess(
+  public_token: string,
+  metadata: {
+    link_session_id: string;
+  }
+) {
+  console.log(`Public Token: ${public_token}
+  Metadata: ${metadata}`);
+  return;
+}
+
+function onExit() {
+  return;
+}
+</script>
